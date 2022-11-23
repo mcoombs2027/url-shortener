7c8c31a4f64c96b1768f9b5d8cebf5abda47c732
Michael Coombs <mcoombs@mb-mcoombs.local>
2022-11-22 16:22:28 -0800

    fixing bug with original url length

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ from datetime import datetime
 
 class ShortUrls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original_url = db.Column(db.String(20), nullable=False, unique=True)
+    original_url = db.Column(db.String(500), nullable=False, unique=True)
     short_id = db.Column(db.String(20), nullable=False, unique=True)
     created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
